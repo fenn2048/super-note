e7a823f2eb8e986ad4b5bc99704e2d621f2ea49c
Richard Fenn <fenn2048@gmail.com>
2026-06-25 16:35:11 +0800

    docker update

--- build_docker.sh
+++ build_docker.sh
@@ -7,10 +7,10 @@ cd frontend/android
 ./build_signed_debug_apk.sh
 cd ../../
 
-# For local docker build compatibility, disable buildkit or clean it.
-# Sometimes overlay issue happens in buildkit. We can prune docker builder.
+# BuildKit is required for multi-arch ARGs (BUILDPLATFORM / TARGETARCH).
+# If you hit overlay issues, run: docker builder prune
 echo "Running docker build..."
-DOCKER_BUILDKIT=0 docker build \
+docker build \
     --build-arg DOCKER_REGISTRY=docker.m.daocloud.io/ \
     --build-arg APK_MIRROR=mirrors.aliyun.com \
     --build-arg NPM_REGISTRY=https://registry.npmmirror.com \
